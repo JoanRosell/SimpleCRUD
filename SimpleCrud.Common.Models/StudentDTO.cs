47fc3cccbe4ddc9891cee09e229897272fa90621
JoanRosell <joan.rosellr@gmail.com>
2021-07-11 12:51:35 +0200

    chore: install automapper on the DA Tier, refs #3

---
+++ SimpleCrud.Common.Models/StudentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleCrud.Common.Models
+{
+    class StudentDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public System.DateTime Birthday { get; set; }
+        public int Age { get; set; }
+    }
+}
