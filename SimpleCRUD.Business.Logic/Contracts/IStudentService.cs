520d1ba9f7f25afa414e5e57ab58b2fd2f90fd15
JoanRosell <joan.rosellr@gmail.com>
2021-07-11 17:51:38 +0200

    feat: add Business Logic Layer and Services Layer, refs #4

---
+++ SimpleCRUD.Business.Logic/Contracts/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using SimpleCrud.Common.Models;
+using System.Collections.Generic;
+
+namespace SimpleCRUD.Business.Logic.Contracts
+{
+    public interface IStudentService
+    {
+        StudentDTO Get(int id);
+
+        IEnumerable<StudentDTO> GetAll();
+
+        void Add(StudentDTO studentData);
+
+        void Update(StudentDTO studentData);
+
+        void Remove(StudentDTO studentData);
+    }
+}
---
+++ SimpleCRUD.Business.Services/Contracts/IStudentRemoteService.cs
@@ -0,0 +1,25 @@
+﻿using SimpleCrud.Common.Models;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace SimpleCRUD.Business.Services
+{
+    [ServiceContract]
+    public interface IStudentRemoteService
+    {
+        [OperationContract]
+        StudentDTO Get(int id);
+
+        [OperationContract]
+        IEnumerable<StudentDTO> GetAll();
+
+        [OperationContract]
+        void Add(StudentDTO studentData);
+
+        [OperationContract]
+        void Update(StudentDTO studentData);
+
+        [OperationContract]
+        void Remove(StudentDTO studentData);
+    }
+}
